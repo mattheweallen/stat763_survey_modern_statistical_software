6d817da3077d0dcea13cd43a86473be3446787bd
Ubuntu <matt@docker-demo-vm.zpimzvslhosurofvu0lqmpvslg.bx.internal.cloudapp.net>
2020-02-04 12:48:12 +0000

    presentation in R markdown

---
+++ docker/docker_presentation.R
@@ -0,0 +1,23 @@
+---
+  title: "Containers, Docker, and R"
+author: "Matt Allen"
+output: ioslides_presentation
+---
+  
+  ```{r, setup, include=FALSE}
+knitr::opts_chunk$set(out.width = '80%', collapse = TRUE, warning=FALSE, message=FALSE)
+```
+
+## What are containers?
+
+- Course overview and mechanics
+- R Basics
+- R and RStudio Workflow Basics
+
+- (Recommended): STAT 345 Homework One (no submission)
+- Readings: R4DS Chapters 4, 6, 8, 27, 29, and 30
+- Other sources: 
+  - [Quick-R Data Management](https://www.statmethods.net/management/index.html)
+- [Quick-R Data Input](https://www.statmethods.net/input/index.html)
+
+
