150311b49f154bbf99e28c265f25b67aa1f27023
Ubuntu <matt@docker-20200208-vm.kk5nk3dtxdmutoyn1maiknhwmh.bx.internal.cloudapp.net>
2020-03-27 18:49:54 +0000

    import data from github

--- assign3/covid_shiny/app.R
+++ assign3/covid_shiny/app.R
@@ -1,8 +1,20 @@
 library(shiny)
+library(readr)
+
+#get data from github
+urlfile <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+confirmed_data <- read_csv(url(urlfile))
+
+urlfile <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+deaths_data <- read_csv(url(urlfile))
+
+urlfile <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+recovered_data <- read_csv(url(urlfile))
+
 
 # Define UI ----
 ui <- fluidPage(
-  "hello matt!" 
+  "how are you matt?" 
 )
 
 # Define server logic ----
